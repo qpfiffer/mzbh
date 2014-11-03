4df053d42d307c38ba5e1c7be533aa3a52c748d6
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-11-02 20:24:01 -0800

    Check gif threads for WEBMs too.

--- src/parse.c
+++ src/parse.c
@@ -32,7 +32,8 @@ ol_stack *parse_catalog_json(const char *all_json, const char board) {
 			const char *post = json_object_get_string(thread, "com");
 
 			if ((file_ext != NULL && strstr(file_ext, "webm")) ||
-				(post != NULL && strcasestr(post, "webm"))) {
+				(post != NULL && strcasestr(post, "webm")) ||
+				(post != NULL && strcasestr(post, "gif"))) {
 				printf("%i probably has a webm. Ext: %s\n%s\n", thread_num, file_ext, post);
 
 				thread_match _match = {
