0085f2fc5d044eabf5940ae0dcb26c2ceacaf5b0
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-11-03 19:25:06 -0800

    Some minimal http parsing.

--- src/server.c
+++ src/server.c
@@ -9,6 +9,32 @@
 #include <string.h>
 #include <unistd.h>
 
+#define MAX_READ_LEN 1024
+
+#include <string.h>
+
+/* Pulled from here: http://stackoverflow.com/a/25705264 */
+char *strnstr(const char *haystack, const char *needle, size_t len)
+{
+	int i;
+	size_t needle_len;
+
+	/* segfault here if needle is not NULL terminated */
+	if (0 == (needle_len = strlen(needle)))
+		return (char *)haystack;
+
+	/* Limit the search if haystack is shorter than 'len' */
+	len = strnlen(haystack, len);
+
+	for (i=0; i<(int)(len-needle_len); i++)
+	{
+		if ((haystack[0] == needle[0]) && (0 == strncmp(haystack, needle, needle_len)))
+			return (char *)haystack;
+
+		haystack++;
+	}
+	return NULL;
+}
 
 const char generic_response[] =
 	"HTTP/1.1 200 OK\r\n"
@@ -19,8 +45,39 @@ const char generic_response[] =
 	"hello";
 
 static int respond(const int accept_fd) {
+	char to_read[MAX_READ_LEN] = {0};
+
+	int rc = recv(accept_fd, to_read, MAX_READ_LEN, 0);
+	if (rc <= 0)
+		goto error;
+
+	/* Find the verb */
+	const char *verb_end = strnstr(to_read, " ", MAX_READ_LEN);
+	if (verb_end == NULL)
+		goto error;
+
+	char verb[16] = {0};
+	const size_t verb_size = verb_end - to_read >= sizeof(verb) ? sizeof(verb) : verb_end - to_read;
+	strncpy(verb, to_read, verb_size);
+
+	if (strncmp(verb, "GET", verb_size) != 0) {
+		printf("Don't know verb %s.\n", verb);
+		goto error;
+	}
+
+	char resource[128] = {0};
+	const char *res_offset = verb_end + sizeof(char);
+	const char *resource_end = strnstr(res_offset, " ", sizeof(resource));
+	if (resource_end == NULL)
+		goto error;
+
+	const size_t resource_size = resource_end - res_offset >= sizeof(resource) ? sizeof(resource) : resource_end - res_offset;
+	strncpy(resource, res_offset, resource_size);
+
+	printf("Verb is %s. Resource is %s.\n", verb, resource);
+
 	size_t resp_size = strlen(generic_response);
-	int rc = send(accept_fd, generic_response, resp_size, 0);
+	rc = send(accept_fd, generic_response, resp_size, 0);
 	if (resp_size != rc)
 		goto error;
 
