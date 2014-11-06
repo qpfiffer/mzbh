245ec9479c5b8de173fbba28038abc2ef87bafb2
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-11-05 16:06:14 -0800

    Better bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 
 PREVIOUS=$(docker ps -a | grep 'waifu.xyz' | awk '{print $1}')
 docker stop $PREVIOUS
 docker rm $PREVIOUS
 
-docker build -t waifu.xyz .
+tar -czh Dockerfile src/ include/ Makefile | docker build -t waifu.xyz -
 docker run --rm -t -v $(pwd)/webms:/app/webms -p 8080:8080 waifu.xyz
