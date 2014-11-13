3eaf988d63e26b4a61d8a00b2f4d3839c1defd70
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-11-13 13:04:52 -0800

    Greshunkel is happening. Again.

--- include/greshunkel.h
+++ include/greshunkel.h
@@ -1,7 +1,9 @@
 // vim: noet ts=4 sw=4
 #pragma once
+#include "stack.h"
 
 #define WISDOM_OF_WORDS 32
+#define MAX_GSHKL_STR_SIZE 32
 
 /* AND HE DECREED: */
 /* STRINGS SHALL NEVER BE MORE THAN 32 OCTETS! */
@@ -9,9 +11,9 @@
 /* NUMBERS SHALL NEVER BE GREATER THAN THE VALUE OF A SINGLE INTEGER! */
 /* VARIABLE NAMES SHALL NEVER BE MORE THAN 32 CHARACTERS! */
 typedef union greshunkel_var {
-	const int num;
-	const char str[32];
-	const union greshunkel_var *arr[8];
+	int num;
+	char str[MAX_GSHKL_STR_SIZE];
+	union greshunkel_var *arr[8];
 } greshunkel_var;
 
 typedef enum {
@@ -21,15 +23,22 @@ typedef enum {
 } greshunkel_type;
 
 typedef struct {
-	const char name[WISDOM_OF_WORDS];
+	char name[WISDOM_OF_WORDS];
 	const greshunkel_type type;
-	const greshunkel_var *value;
+	greshunkel_var value;
 } greshunkel_tuple;
 
 typedef struct {
-	greshunkel_tuple *values;
+	ol_stack *values;
 } greshunkel_ctext;
 
-int gshkl_init_context(greshunkel_ctext *ctext);
-int gshkl_add_variable(const char name[WISDOM_OF_WORDS], const greshunkel_tuple *new_var);
+/* Build and destroy contexts: */
+greshunkel_ctext *gshkl_init_context();
 int gshkl_free_context(greshunkel_ctext *ctext);
+
+/* Add things to the contexts: */
+int gshkl_add_string(greshunkel_ctext *ctext, const char name[WISDOM_OF_WORDS], const char *value);
+int gshkl_add_num(greshunkel_ctext *ctext, const char name[WISDOM_OF_WORDS], const int value);
+
+/* Render a string buffer: */
+const char *gshkl_render(const greshunkel_ctext *ctext, const char *to_render);
--- src/greshunkel.c
+++ src/greshunkel.c
@@ -1,13 +1,62 @@
 // vim: noet ts=4 sw=4
+#include <assert.h>
 #include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+
 #include "greshunkel.h"
 
-int gshkl_init_context(greshunkel_ctext *ctext) {
-	ctext = calloc(1, sizeof(greshunkel_ctext));
+static const char variable_regex[] = "xXx (?P<variable>[a-zA-Z_0-9\\$]+) xXx";
+static const char loop_regex[] = "xXx LOOP (?P<variable>[a-zA-S_]+) (?P<iter_list>[a-zA-S_\\$]+) xXx(?P<subloop>.*)xXx BBL xXx";
+
+greshunkel_ctext *gshkl_init_context() {
+	greshunkel_ctext *ctext = calloc(1, sizeof(greshunkel_ctext));
+	ctext->values = calloc(1, sizeof(ol_stack));
+	return ctext;
+}
+
+int gshkl_add_string(greshunkel_ctext *ctext, const char name[WISDOM_OF_WORDS], const char value[MAX_GSHKL_STR_SIZE]) {
+	assert(ctext != NULL);
+
+	/* Create a new tuple to hold type and name and shit. */
+	greshunkel_tuple _stack_tuple = {
+		.name = {0},
+		.type = GSHKL_STR,
+		.value =  {0}
+	};
+	strncpy(_stack_tuple.name, name, WISDOM_OF_WORDS);
+
+	/* Copy the value of the string into the var object: */
+	greshunkel_var _stack_var = {0};
+	strncpy(_stack_var.str, value, MAX_GSHKL_STR_SIZE);
+
+	/* Copy the var object itself into the tuple's var space: */
+	memcpy(&_stack_tuple.value, &_stack_var, sizeof(greshunkel_var));
+
+	/* Copy the stack allocated object onto the heap (so we can have const stuff) */
+	greshunkel_tuple *new_tuple = calloc(1, sizeof(greshunkel_tuple));
+	memcpy(new_tuple, &_stack_tuple, sizeof(greshunkel_tuple));
+
+	/* Push that onto our values stack. */
+	spush(&ctext->values, new_tuple);
+	return 0;
+}
+
+int gshkl_add_num(greshunkel_ctext *ctext, const char name[WISDOM_OF_WORDS], const int value) {
+	assert(ctext != NULL);
 	return 0;
 }
 
 int gshkl_free_context(greshunkel_ctext *ctext) {
+	while (ctext->values->next != NULL) {
+		greshunkel_tuple *next = (greshunkel_tuple *)spop(&ctext->values);
+		free(next);
+	}
+	free(ctext->values);
 	free(ctext);
 	return 0;
+}
+
+const char *gshkl_render(const greshunkel_ctext *ctext, const char *to_render) {
+	return NULL;
 }
