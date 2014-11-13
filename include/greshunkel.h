8141f75dcc70c5a3cc4616d883122fd29e387f15
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-11-13 11:23:43 -0800

    its-happening.shithouse.tv

--- include/greshunkel.h
+++ include/greshunkel.h
@@ -1,14 +1,17 @@
 // vim: noet ts=4 sw=4
 #pragma once
 
+#define WISDOM_OF_WORDS 32
+
 /* AND HE DECREED: */
 /* STRINGS SHALL NEVER BE MORE THAN 32 OCTETS! */
 /* ARRAYS OF MORE VARIABLES WILL NEVER BE MORE THAN 8 ITEMS IN LENGTH! */
 /* NUMBERS SHALL NEVER BE GREATER THAN THE VALUE OF A SINGLE INTEGER! */
+/* VARIABLE NAMES SHALL NEVER BE MORE THAN 32 CHARACTERS! */
 typedef union greshunkel_var {
 	const int num;
 	const char str[32];
-	const greshunkel_var arr[8];
+	const union greshunkel_var *arr[8];
 } greshunkel_var;
 
 typedef enum {
@@ -18,7 +21,7 @@ typedef enum {
 } greshunkel_type;
 
 typedef struct {
-	const char *name;
+	const char name[WISDOM_OF_WORDS];
 	const greshunkel_type type;
 	const greshunkel_var *value;
 } greshunkel_tuple;
@@ -28,4 +31,5 @@ typedef struct {
 } greshunkel_ctext;
 
 int gshkl_init_context(greshunkel_ctext *ctext);
+int gshkl_add_variable(const char name[WISDOM_OF_WORDS], const greshunkel_tuple *new_var);
 int gshkl_free_context(greshunkel_ctext *ctext);
--- src/greshunkel.c
+++ src/greshunkel.c
@@ -1,2 +1,13 @@
 // vim: noet ts=4 sw=4
+#include <stdlib.h>
 #include "greshunkel.h"
+
+int gshkl_init_context(greshunkel_ctext *ctext) {
+	ctext = calloc(1, sizeof(greshunkel_ctext));
+	return 0;
+}
+
+int gshkl_free_context(greshunkel_ctext *ctext) {
+	free(ctext);
+	return 0;
+}
