9c53aed5482ebd46e81e2488b1a5382f9e3605b2
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-11-27 09:59:18 -0800

    Forgot this file.

---
+++ include/common_defs.h
@@ -0,0 +1,13 @@
+// vim: noet ts=4 sw=4
+#pragma once
+
+/* Size of BMW hash */
+#define IMAGE_HASH_SIZE 512
+#define HASH_ARRAY_SIZE IMAGE_HASH_SIZE/8
+
+/* Namespaces for when we create our keys */
+#define WAIFU_NMSPC waifu
+#define WEBM_NMSPC webm
+
+#define MAX_IMAGE_FILENAME_SIZE 1024
+#define MAX_BOARD_NAME_SIZE 16
