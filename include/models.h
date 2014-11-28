52e0e57acd9d0b634d6cf32eb94feb218559ee47
Quinlan Pfiffer <qpfiffer@gmail.com>
2014-11-28 15:55:22 -0800

    comment and consistency.

--- include/models.h
+++ include/models.h
@@ -3,16 +3,21 @@
 #include <time.h>
 #include "common_defs.h"
 
+/* The unsigned chars in the struct are used to null terminate the
+ * strings while still allowing us to use 'sizeof(webm.file_hash)'.
+ */
 typedef struct webm {
 	unsigned char is_alias;
+
 	char file_hash[HASH_IMAGE_STR_SIZE];
-	unsigned char _null_term_hax;
+	unsigned char _null_term_hax_1;
 
 	char filename[MAX_IMAGE_FILENAME_SIZE];
 	unsigned char _null_term_hax_2;
 
 	char board[MAX_BOARD_NAME_SIZE];
 	unsigned char _null_term_hax_3;
+
 	time_t created_at;
 	size_t size;
 } webm;
