c1ebf8687ac2adaf82a77cfb7aa42dd14fd96542
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-01-28 21:02:55 -0800

    Retry loop for webm/thumbnail downloading.

--- src/main.c
+++ src/main.c
@@ -169,157 +169,129 @@ error:
 	return NULL;
 }
 
-int download_images() {
+const int download_image(const post_match *p_match) {
 	int thumb_request_fd = 0;
 	int image_request_fd = 0;
 	unsigned char *raw_thumb_resp = NULL;
 	unsigned char *raw_image_resp = NULL;
 	FILE *thumb_file = NULL;
 	FILE *image_file = NULL;
-	post_match *p_match = NULL;
 
-	struct stat st = {0};
-	if (stat(webm_location(), &st) == -1) {
-		log_msg(LOG_WARN, "Creating webms directory %s.", webm_location());
-		mkdir(webm_location(), 0755);
-	}
-
-	ol_stack *images_to_download = NULL;
-	images_to_download = build_thread_index();
-	if (images_to_download == NULL) {
-		log_msg(LOG_WARN, "No images to download.");
+	thumb_request_fd = connect_to_host(FOURCHAN_THUMBNAIL_HOST);
+	if (thumb_request_fd < 0) {
+		log_msg(LOG_ERR, "Could not connect to thumbnail host.");
 		goto error;
 	}
 
-	/* Now actually download the images. */
-	while (images_to_download->next != NULL) {
-		thumb_request_fd = connect_to_host(FOURCHAN_THUMBNAIL_HOST);
-		if (thumb_request_fd < 0) {
-			log_msg(LOG_ERR, "Could not connect to thumbnail host.");
-			goto error;
-		}
-
-		image_request_fd = connect_to_host(FOURCHAN_IMAGE_HOST);
-		if (image_request_fd < 0) {
-			log_msg(LOG_ERR, "Could not connect to image host.");
-			goto error;
-		}
-
-		p_match = (post_match *)spop(&images_to_download);
-
-		char image_filename[MAX_IMAGE_FILENAME_SIZE] = {0};
-		int should_skip = get_non_colliding_image_file_path(image_filename, p_match);
-
-		/* We already have that file. */
-		if (should_skip) {
-			free(p_match);
-			continue;
-		}
-
-		char thumb_filename[MAX_IMAGE_FILENAME_SIZE] = {0};
-		get_thumb_filename(thumb_filename, p_match);
-
-		log_msg(LOG_INFO, "Downloading %s%.*s...", p_match->filename, 5, p_match->file_ext);
-
-		/* Build and send the thumbnail request. */
-		char thumb_request[256] = {0};
-		snprintf(thumb_request, sizeof(thumb_request), THUMB_REQUEST,
-				p_match->board, p_match->post_number);
-		int rc = send(thumb_request_fd, thumb_request, strlen(thumb_request), 0);
-		if (rc != strlen(thumb_request)) {
-			log_msg(LOG_ERR, "Could not send all bytes to host while requesting thumbnail.");
-			goto error;
-		}
-
-		/* Build and send the image request. */
-		char image_request[256] = {0};
-		snprintf(image_request, sizeof(image_request), IMAGE_REQUEST,
-				p_match->board, p_match->post_number, (int)sizeof(p_match->file_ext), p_match->file_ext);
-		rc = send(image_request_fd, image_request, strlen(image_request), 0);
-		if (rc != strlen(image_request)) {
-			log_msg(LOG_ERR, "Could not send all bytes to host while requesting image.");
-			goto error;
-		}
-
-		size_t thumb_size = 0, image_size = 0;
-		raw_thumb_resp = receive_http(thumb_request_fd, &thumb_size);
-		raw_image_resp = receive_http(image_request_fd, &image_size);
-
-		if (thumb_size <= 0 || image_size <= 0) {
-			/* 4chan cut us off. This happens sometimes. Just sleep for a bit. */
-			log_msg(LOG_WARN, "Hit API cutoff or whatever. Sleeping.");
-			sleep(30);
-			free(p_match);
-			free(raw_thumb_resp);
-			free(raw_image_resp);
-			continue;
-		}
-
-		if (raw_thumb_resp == NULL) {
-			log_msg(LOG_ERR, "No thumbnail received.");
-			goto error;
-		}
-
-		if (raw_image_resp == NULL) {
-			log_msg(LOG_ERR, "No image received.");
-			goto error;
-		}
-
-		/* Write thumbnail to disk. */
-		thumb_file = fopen(thumb_filename, "wb");
-		if (!thumb_file || ferror(thumb_file)) {
-			log_msg(LOG_ERR, "Could not open thumbnail file: %s", thumb_filename);
-			perror(NULL);
-			goto error;
-		}
-		const size_t rt_written = fwrite(raw_thumb_resp, 1, thumb_size, thumb_file);
-		log_msg(LOG_INFO, "Wrote %i bytes of thumbnail to disk.", rt_written);
-		if (rt_written <= 0 || ferror(thumb_file)) {
-			log_msg(LOG_WARN, "Could not write thumbnail to disk: %s", thumb_filename);
-			perror(NULL);
-			goto error;
-		}
-		fclose(thumb_file);
-		thumb_file = 0;
-
-		image_file = fopen(image_filename, "wb");
-		if (!image_file || ferror(image_file)) {
-			log_msg(LOG_ERR, "Could not open image file: %s", image_filename);
-			perror(NULL);
-			goto error;
-		}
-		const size_t iwritten = fwrite(raw_image_resp, 1, image_size, image_file);
-		if (iwritten <= 0 || ferror(image_file)) {
-			log_msg(LOG_WARN, "Could not write image to disk: %s", image_filename);
-			perror(NULL);
-			goto error;
-		}
-		log_msg(LOG_INFO, "Wrote %i bytes of image to disk.", iwritten);
-		fclose(image_file);
-		image_file = 0;
-
-		char fname_plus_extension[MAX_IMAGE_FILENAME_SIZE] = {0};
-		get_non_colliding_image_filename(fname_plus_extension, p_match);
-		int added = add_image_to_db(image_filename, fname_plus_extension, p_match->board);
-		if (!added) {
-			log_msg(LOG_WARN, "Could not add image to database. Continuing...");
-		}
-
-		/* Don't need the post match anymore: */
-		free(p_match);
-		p_match = NULL;
-		free(raw_image_resp);
-		raw_image_resp = NULL;
-		free(raw_thumb_resp);
-		raw_thumb_resp = NULL;
-
-		close(thumb_request_fd);
-		close(image_request_fd);
+	image_request_fd = connect_to_host(FOURCHAN_IMAGE_HOST);
+	if (image_request_fd < 0) {
+		log_msg(LOG_ERR, "Could not connect to image host.");
+		goto error;
 	}
-	free(images_to_download);
-	log_msg(LOG_INFO, "Downloaded all images.");
 
-	return 0;
+	char image_filename[MAX_IMAGE_FILENAME_SIZE] = {0};
+	int should_skip = get_non_colliding_image_file_path(image_filename, p_match);
+
+	/* We already have that file. */
+	if (should_skip) {
+		return 1;
+	}
+
+	char thumb_filename[MAX_IMAGE_FILENAME_SIZE] = {0};
+	get_thumb_filename(thumb_filename, p_match);
+
+	log_msg(LOG_INFO, "Downloading %s%.*s...", p_match->filename, 5, p_match->file_ext);
+
+	/* Build and send the thumbnail request. */
+	char thumb_request[256] = {0};
+	snprintf(thumb_request, sizeof(thumb_request), THUMB_REQUEST,
+			p_match->board, p_match->post_number);
+	int rc = send(thumb_request_fd, thumb_request, strlen(thumb_request), 0);
+	if (rc != strlen(thumb_request)) {
+		log_msg(LOG_ERR, "Could not send all bytes to host while requesting thumbnail.");
+		goto error;
+	}
+
+	/* Build and send the image request. */
+	char image_request[256] = {0};
+	snprintf(image_request, sizeof(image_request), IMAGE_REQUEST,
+			p_match->board, p_match->post_number, (int)sizeof(p_match->file_ext), p_match->file_ext);
+	rc = send(image_request_fd, image_request, strlen(image_request), 0);
+	if (rc != strlen(image_request)) {
+		log_msg(LOG_ERR, "Could not send all bytes to host while requesting image.");
+		goto error;
+	}
+
+	size_t thumb_size = 0, image_size = 0;
+	raw_thumb_resp = receive_http(thumb_request_fd, &thumb_size);
+	raw_image_resp = receive_http(image_request_fd, &image_size);
+
+	if (thumb_size <= 0 || image_size <= 0) {
+		free(raw_thumb_resp);
+		free(raw_image_resp);
+		return 0;
+	}
+
+	if (raw_thumb_resp == NULL) {
+		log_msg(LOG_ERR, "No thumbnail received.");
+		goto error;
+	}
+
+	if (raw_image_resp == NULL) {
+		log_msg(LOG_ERR, "No image received.");
+		goto error;
+	}
+
+	/* Write thumbnail to disk. */
+	thumb_file = fopen(thumb_filename, "wb");
+	if (!thumb_file || ferror(thumb_file)) {
+		log_msg(LOG_ERR, "Could not open thumbnail file: %s", thumb_filename);
+		perror(NULL);
+		goto error;
+	}
+	const size_t rt_written = fwrite(raw_thumb_resp, 1, thumb_size, thumb_file);
+	log_msg(LOG_INFO, "Wrote %i bytes of thumbnail to disk.", rt_written);
+	if (rt_written <= 0 || ferror(thumb_file)) {
+		log_msg(LOG_WARN, "Could not write thumbnail to disk: %s", thumb_filename);
+		perror(NULL);
+		goto error;
+	}
+	fclose(thumb_file);
+	thumb_file = 0;
+
+	image_file = fopen(image_filename, "wb");
+	if (!image_file || ferror(image_file)) {
+		log_msg(LOG_ERR, "Could not open image file: %s", image_filename);
+		perror(NULL);
+		goto error;
+	}
+	const size_t iwritten = fwrite(raw_image_resp, 1, image_size, image_file);
+	if (iwritten <= 0 || ferror(image_file)) {
+		log_msg(LOG_WARN, "Could not write image to disk: %s", image_filename);
+		perror(NULL);
+		goto error;
+	}
+	log_msg(LOG_INFO, "Wrote %i bytes of image to disk.", iwritten);
+	fclose(image_file);
+	image_file = 0;
+
+	char fname_plus_extension[MAX_IMAGE_FILENAME_SIZE] = {0};
+	get_non_colliding_image_filename(fname_plus_extension, p_match);
+	int added = add_image_to_db(image_filename, fname_plus_extension, p_match->board);
+	if (!added) {
+		log_msg(LOG_WARN, "Could not add image to database. Continuing...");
+	}
+
+	/* Don't need the post match anymore: */
+	free(raw_image_resp);
+	raw_image_resp = NULL;
+	free(raw_thumb_resp);
+	raw_thumb_resp = NULL;
+
+	close(thumb_request_fd);
+	close(image_request_fd);
+
+	return 1;
 
 error:
 	free(raw_thumb_resp);
@@ -330,19 +302,48 @@ error:
 	if (image_request_fd)
 		close(image_request_fd);
 
-	if (images_to_download != NULL) {
-		while (images_to_download->next != NULL) {
-			post_match *_match = (post_match *)spop(&images_to_download);
-			free(_match);
-		}
-		free(images_to_download);
-	}
-
 	if (thumb_file != NULL)
 		fclose(thumb_file);
+
 	if (image_file != NULL)
 		fclose(image_file);
-	return -1;
+	return 0;
+}
+
+int download_images() {
+	struct stat st = {0};
+	if (stat(webm_location(), &st) == -1) {
+		log_msg(LOG_WARN, "Creating webms directory %s.", webm_location());
+		mkdir(webm_location(), 0755);
+	}
+
+	ol_stack *images_to_download = NULL;
+	images_to_download = build_thread_index();
+	if (images_to_download == NULL) {
+		log_msg(LOG_WARN, "No images to download.");
+		return -1;
+	}
+
+	/* Now actually download the images. */
+	while (images_to_download->next != NULL) {
+		post_match *p_match = (post_match *)spop(&images_to_download);
+		int i;
+		const int max_retries = 5;
+		for (i = 0; i < max_retries; i ++) {
+			if (download_image(p_match))
+				break;
+
+			/* Connections are flaky. */
+			log_msg(LOG_WARN, "(%i/%i): Could not download image. Retrying after sleep.", i + 1, max_retries);
+			sleep(10);
+		}
+		free(p_match);
+	}
+
+	free(images_to_download);
+	log_msg(LOG_INFO, "Downloaded all images.");
+
+	return 0;
 }
 
 void term(int signum) {
