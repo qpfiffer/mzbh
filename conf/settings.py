ec14481de5207003bb1a704c26ae20a28ae0c3be
Quinlan Pfiffer <qpfiffer@gmail.com>
2016-05-13 14:23:09 -0700

    WEBMS

--- conf/settings.py
+++ conf/settings.py
@@ -123,5 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
+os.path.join(BASE_DIR, "webms")]
 STATIC_URL = '/static/'
