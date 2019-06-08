44d73686b3c4045a09fc5df88e720cdc57240f62
Quinlan Pfiffer <qpfiffer@gmail.com>
2019-06-08 09:02:00 -0700

    sql

--- sql/most_popular_webms_by_alias_count.sql
+++ sql/most_popular_webms_by_alias_count.sql
@@ -1,4 +1,4 @@
-SELECT webms.id, count(webm_aliases.id), webms.file_path
+SELECT webms.id, count(webm_aliases.id), 'http://tired/slurp/' || webms.board || '/' || webms.filename
 FROM webms left join webm_aliases ON webms.id = webm_aliases.webm_id
 GROUP BY webms.id
 ORDER BY count(webm_aliases.id) DESC;
