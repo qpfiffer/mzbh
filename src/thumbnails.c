014cee3b0c3f66aef979b4b3f656e9819a83eb99
Quinlan Pfiffer <qpfiffer@gmail.com>
2019-06-13 17:56:06 -0700

    Lost motivation.

--- src/thumbnails.c
+++ src/thumbnails.c
@@ -1,5 +1,8 @@
 // vim: noet ts=4 sw=4
+// From here, basically: https://ffmpeg.org/doxygen/0.6/output-example_8c-source.html
 #include <libavformat/avformat.h>
+
+#include <38-moths/38-moths.h>
 
 #include "thumbnails.h"
 #include "utils.h"
@@ -12,11 +15,30 @@ int generate_thumbnail(const char webm_path[static MAX_IMAGE_FILENAME_SIZE],
 
 	av_register_all();
 
-	UNUSED(webm_path);
+	oc = avformat_alloc_context();
+	if (!oc) {
+		m38_log_msg(LOG_ERR, "Could not allocate AVFormat.");
+		return 1;
+	}
+	oc->oformat = fmt;
+	strncpy(oc->filename, webm_path, sizeof(oc->filename));
+
+	video_st = add_video_stream(oc, fmt->video_codec);
+
+	if (av_set_parameters(oc, NULL) < 0) {
+		m38_log_msg(LOG_ERR, "Invalid output parameters.");
+		return 1;
+	}
+
+	dump_format(oc, 0, webm_path, 1);
+
+	open_video(oc, video_st);
+	if (url_fopen(&oc->pb, webm_path, URL_WRONLY) < 0) {
+		m38_log_msg(LOG_ERR, "Could not open '%s'", webm_path);
+		return 1;
+	}
+
 	UNUSED(thumbnail_path);
-	UNUSED(fmt);
-	UNUSED(oc);
-	UNUSED(video_st);
 
 	return 0;
 }
