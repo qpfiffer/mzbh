c7b4a7d211cc59555520e0b7a1b10a175730a0a4
Quinlan Pfiffer <qpfiffer@gmail.com>
2020-04-01 15:36:58 -0700

    Get counts.

--- mzbh/blueprints/main.py
+++ mzbh/blueprints/main.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, g, render_template
 
 from mzbh.database import db
-from mzbh.models import Category
+from mzbh.models import Category, Webm, WebmAlias, Post
 
 blueprint = Blueprint('main', __name__)
 
 @blueprint.route("/", methods=("GET",))
 def root():
     d = {
-        "webm_count": 0,
-        "alias_count": 0,
-        "post_count": 0,
+        "webm_count": Webm.query.count(),
+        "alias_count": WebmAlias.query.count(),
+        "post_count": Post.query.count(),
         "categories": Category.query.all(),
     }
 
