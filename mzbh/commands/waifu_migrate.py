0789750e4d5b935b6f768a61c991d3a2002dd809
Quinlan Pfiffer <qpfiffer@gmail.com>
2020-04-12 19:46:40 -0700

    Random stuff

--- mzbh/commands/waifu_migrate.py
+++ mzbh/commands/waifu_migrate.py
@@ -11,25 +11,30 @@ from mzbh.filters import _get_thumb_filename
 from mzbh.models import Category, Host, Post, Thread, Webm, WebmAlias
 from mzbh.utils import get_or_create
 
-conn = psycopg2.connect("dbname=waifu")
-conn.autocommit = False
+waifu_conn = psycopg2.connect("dbname=waifu")
+waifu_conn.autocommit = False
+
+mzbh_conn = psycopg2.connect("dbname=mzbh")
+mzbh_conn.autocommit = False
+
+# 1. Alter table <foo> add old_id = waifu_table.id;
 
 thread_fetch_sql = 'SELECT * FROM threads;'
-def _do_threads(cur):
+def _do_threads(mzbh_cur, waifu_cur):
+    mzbh_cur.execute("ALTER TABLE mzbh.thread ADD COLUMN old_id INTEGER;")
     host, _ = get_or_create(Host, name="4chan")
-    cur.execute(thread_fetch_sql)
-    row = cur.fetchone()
+    waifu_cur.execute(thread_fetch_sql)
+    row = waifu_cur.fetchone()
     while row:
         board = row["board"]
         subject = row["subject"]
         category, _ = get_or_create(Category, name=board, host_id=host.id)
         thread_obj, _ = get_or_create(Thread, category_id=category.id, thread_ident=t_num, subject=subject)
-        # Aw shit we have to be able to tie the old IDs back to the webms/aliases.
-        # Probably with some temporary "old_id" column so we can correlate everything.
-        row = cur.fetchone()
+        # HAve to add old_id to the ORM thinger, too.
+        row = waifu_cur.fetchone()
 
 post_fetch_sql = 'SELECT * FROM posts;'
-def _do_posts(cur):
+def _do_posts(mzbh_cur, waifu_cur):
     cur.execute(post_fetch_sql)
     row = cur.fetchone()
     while row:
@@ -37,7 +42,7 @@ def _do_posts(cur):
         row = cur.fetchone()
 
 webm_fetch_sql = 'SELECT * FROM webms;'
-def _do_webms(cur):
+def _do_webms(mzbh_cur, waifu_cur):
     cur.execute(webm_fetch_sql)
     row = cur.fetchone()
     while row:
@@ -45,7 +50,7 @@ def _do_webms(cur):
         row = cur.fetchone()
 
 webm_alias_fetch_sql = 'SELECT * FROM webm_aliases;'
-def _do_aliases(cur):
+def _do_aliases(mzbh_cur, waifu_cur):
     cur.execute(webm_alias_fetch_sql)
     row = cur.fetchone()
     while row:
@@ -56,11 +61,15 @@ def _do_aliases(cur):
 @with_appcontext
 def waifu_migrate():
     try:
-        with conn.cursor() as cur:
-            _do_threads(cur)
-            _do_posts(cur)
-            _do_webms(cur)
-            _do_aliases(cur)
+        with waifu_conn.cursor() as waifu_cur, mzbh_conn.cursor() as mzbh_cur:
+            mzbh_cur.execute("ALTER TABLE mzbh.thread ADD COLUMN old_id INTEGER;")
+            # Repeat the add column for all tables ^^^
+            _do_threads(mzbh_cur, waifu_cur)
+            _do_posts(mzbh_cur, waifu_cur)
+            _do_webms(mzbh_cur, waifu_cur)
+            _do_aliases(mzbh_cur, waifu_cur)
+            cur.execute("ALTER TABLE mzbh.thread DROP COLUMN old_id;")
+            # Repeat the drop column for all tables ^^^
     except Exception as e:
         print("Exception: {e}")
         conn.rollback()
